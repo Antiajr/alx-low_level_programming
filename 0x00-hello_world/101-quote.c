e6fb4065fc15b1caed3844615fa9ab9b9fdc4f86
Antiajr <antiahope15@gmail.com>
2022-12-08 20:58:28 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,10 +11,8 @@
  */
 
 int main(void)
-
 {
 	fprintf(stderr,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
-
+		"and that place of art is useful\" - Dora korpar, 2015-10-19\n");
+	return(1);
 }
