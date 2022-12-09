0cd08d2b807a8f6e67188caaa5cc5364272d2f4d
Antiajr <antiahope15@gmail.com>
2022-12-08 20:45:09 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *
+ * main-it prints to standard error
+ *  
+ *
+ * Return: 1 if successful
+ *   
+ */
+
+int main(void)
+
+{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
