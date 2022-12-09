05154a145628e441192d25fbebdcaa1a5fded1d4
Antiajr <antiahope15@gmail.com>
2022-12-08 20:11:11 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main- it runs the printf functions which prints it code block
+ *
+ * 
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+
+{
+		
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
