7975d6e6546c72e67ff75d5630b06ffd38756cb0
Antiajr <antiahope15@gmail.com>
2022-12-10 01:10:43 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- * main- it runs the printf functions which prints it code block
- *
- * 
- *
- * Return: 0 if successful
- *
+ * main - prints the phrase with proper grammer.
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
-
 {
-		
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art, \n");
 	return (0);
-
 }
