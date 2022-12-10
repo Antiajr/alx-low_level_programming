92b157a899fde572d81a08a38ba40cd9df46abde
Antiajr <antiahope15@gmail.com>
2022-12-10 01:30:00 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,12 @@
 #include <unistd.h>
 
 /**
- *
- * main-it prints to standard error
- *  
- *
- * Return: 1 if successful
- *   
+ * main- prints exactly and that piece of art is useful
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	fprintf(stderr,
-		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
