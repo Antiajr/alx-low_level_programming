53411755720df4485557a93f1ebdc441fe93ec57
Antiajr <antiahope15@gmail.com>
2022-12-19 18:20:43 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "2-strlen.c"
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int test;
+
+	for (test = _strlen(s) -1; test >= 0; test--)
+	{
+		_putchar(*(s + test));
+	}
+	_putchar('\n');
+}
