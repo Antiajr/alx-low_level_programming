64a41b969e0ab7759a02979a6e7396a54385ec97
Antiajr <antiahope15@gmail.com>
2022-12-19 23:52:21 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 {
 	int test;
 
-	for (test = _strlen(s) -1; test >= 0; test--)
+	for (test = _strlen(s) - 1; test >= 0; test--)
 	{
 		_putchar(*(s + test));
 	}
-	
+
 	_putchar('\n');
 }
